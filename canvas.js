9ac4520e967980b2ee735ea6ddfa55cce4e4a026
finn <fwander@wpi.edu>
2020-12-22 02:35:49 -0500

    added selecting notes and dragging/resizing multiple notes

--- canvas.js
+++ canvas.js
@@ -12,34 +12,33 @@ function Midi2Freq(M){
 const bpm = 500;
 const width = 3000;
 const slotWidth = (width / (8 * 4 * 4));
-const numNotes = 100;
 const noteHeight = 21;
 const leftOffset = 50;
 const topOffset = 20;
 const slots = document.getElementById('slots');
+const lowestNote = 36;
+const highestNote = 73;
+var numNotes = highestNote - lowestNote;
 var playing = false;
 var beats = 1;
 var control = false;
 var synth;
+var selected = [];
 
 class Note{
-	constructor(note,beats,vel,offset,arr){
+	constructor(note,beats,vel,offset,div){
 		this.note = note;
 		this.beats = beats;
 		this.vel = vel;
 		this.offset = offset;
-		if (arr == undefined){
-			this.arr = arrMaker(this, sineWaveAt);
-		}
-		else{
-			this.arr = arr;
-		}
+        this.div = div;
+        this.arr = arrMaker(this, sineWaveAt);
 	}
 	copy(){
-		return new Note(this.note, this.beats, this.vel, this.offset);
+		return new Note(this.note, this.beats, this.vel, this.offset, this.div);
 	}
 	shortCopy(){
-		return new Note(this.note, 1, this.vel, this.offset);
+		return new Note(this.note, 1, this.vel, this.offset, this.div);
 	}
 }; 
 
@@ -123,16 +122,17 @@ function env(a, d, s, r, t, len, rec){
 
 function createNote(x, y, note, beats, vel){ //
 	var offset = Math.floor(x/slotWidth);
-	n = new Note(note, beats, vel, offset);
+	var div = createDiv('note', "o" + offset + "n" + note, x, y, beats * slotWidth, 16);
+	n = new Note(note, beats, vel, offset, div);
 	if (!playing){
 		playSound(n.shortCopy().arr);
 	}
 	addNote(n, notes);
-	var div = createDiv('note', "o" + offset + "n" + note, x, y, beats * slotWidth, 16);
 	slots.appendChild(div);
     var subdiv = createDiv('drag', "d", 0,0,6,17);
     div.appendChild(subdiv);
     subdiv.addEventListener("mousedown", resize);	
+    div.addEventListener("mousedown", drag);	
     subdiv.ondragstart = function () {
           return false;
     };
@@ -178,12 +178,10 @@ function setUp(){ // creates all of the divs needed for workspace
 	function addSlots(){ // add the places for notes to go "slots"
 		var cs = ["#222", "#555"];
 		var slots = document.getElementById('slots');
-		for (var i = 0 ; i < 100; i++){
-			//for (var x = 0; x < width/slotWidth; x++){
-				var div = createDiv('slot', "n" + (numNotes - i), leftOffset + 0 *slotWidth, i * noteHeight + topOffset, width, 17); 
-				div.style.background = cs[i%2];
-				slots.appendChild(div);
-			//}
+		for (var i = 0 ; i < highestNote - lowestNote; i++){
+            var div = createDiv('slot', "n" + (numNotes - i + lowestNote), leftOffset + 0 *slotWidth, i * noteHeight + topOffset, width, 17); 
+            div.style.background = cs[i%2];
+            slots.appendChild(div);
 		}
 	}
 	function addBars(num, elem, rec, w, offset){ // create visual seperation of mesures
@@ -240,7 +238,7 @@ release.oninput = function() {
       synth.r = (this.value)/100;
 }
 
-var notes = [new Note(0,1,0,0)];
+var notes = [new Note(0,1,0,0,null)];
 setUp();
 
 //event handlers
@@ -258,6 +256,14 @@ function onPlay() {
     }
 }
 
+function deselect(){
+    console.log(selected);
+    for(var i = 0; i < selected.length; i++){
+        selected[i].div.style.background = "#bb5";
+    }
+    selected = [];
+}
+
 function targetToNote(e){
     offset = parseInt(e.id.substring(1));
     note = parseInt(e.id.substring(e.id.indexOf("n")+1));
@@ -267,27 +273,48 @@ function targetToNote(e){
 document.getElementById('container').onmousedown = function clickEvent(e) {
     if (control){
         offsetX = e.pageX + slots.scrollLeft - 7;
-        div = createDiv("select", "temp", offsetX, e.target.offsetTop, 10, 10);
+        offsetY = (e.pageY + slots.scrollTop - topOffset - 28);
+        div = createDiv("select", "temp", offsetX, offsetY, 10, 10);
         slots.appendChild(div);
         startX = e.screenX;
         startY = e.screenY;
         document.addEventListener('mousemove', onMouseMove, false);
         document.addEventListener('mouseup', onMouseUp, false);
+        
+        var maxNote = numNotes + 3 + lowestNote - Math.floor((e.pageY + slots.scrollTop - 28)/noteHeight);
+        var minOffset = Math.floor((e.pageX + slots.scrollLeft - 7)/slotWidth);
         function onMouseMove(e){
             div.style.width = e.screenX - startX + "px";
             div.style.height = e.screenY - startY + "px";
         }
         function onMouseUp(e){
+            
+            div.remove();
+            var minNote = numNotes + 1 + lowestNote - Math.floor((e.pageY + slots.scrollTop - 28)/noteHeight);
+            var maxOffset = Math.floor((e.pageX + slots.scrollLeft - 7)/slotWidth) + 1;
+            console.log(minOffset);
+            console.log(maxOffset);
+            selected = [];
+            for (var i = 1; i < notes.length; i++){
+                var note = notes[i];
+                note.div.style.background = "#bb5";
+                if (note.note > minNote && note.note < maxNote && note.offset + note.beats > minOffset && note.offset < maxOffset){
+                    console.log("hello");
+                    note.div.style.background = "#000";
+                    selected.push(note);
+                }
+            }
             document.removeEventListener('mouseup', onMouseUp);
             document.removeEventListener('mousemove', onMouseMove);
         }
     }
 }
+document.getElementById('container').ondragstart = function() { return false; };
 document.getElementById('container').onclick = function clickEvent(e) {
-    // e = Mouse click event.
     console.log(slots.scrollLeft);
     switch(e.target.id[0]) {
     case "n": // div id starts with n, indecating a slot
+        deselect();
         var rect = e.target; 
         createNote(Math.floor((e.pageX + slots.scrollLeft - 7)/slotWidth)*slotWidth+3, rect.offsetTop, parseInt(e.target.id.substring(1)), beats, .1);
         break;
@@ -315,18 +342,73 @@ function resize(e){
     var target;
     var tnote;
     target = e.target.parentNode;
-    var startWidth = parseFloat(target.style.width);
     var initX = e.clientX;
     tnote = notes[targetToNote(target)];
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
+    var sel = [tnote];
+    if (selected.length != 0){
+       sel = selected; 
+    }
+    var startwidth = [];
+    for (var i = 0; i < sel.length; i++){
+        startwidth.push(sel[i].beats * slotWidth); 
+    }
+    console.log(startwidth);
     function onMouseMove(e){
-        var newWidth = (startWidth + e.clientX - initX);
-        newWidth = Math.floor((newWidth/slotWidth)+.5);
-        tnote.beats = newWidth;
-        beats = newWidth;
-        newWidth = newWidth*slotWidth; 
-        target.style.width = newWidth + "px";
+        for (var i = 0; i < sel.length; i++){
+            var newWidth = (startwidth[i] + e.clientX - initX);
+            newWidth = Math.floor((newWidth/slotWidth)+.5);
+            sel[i].beats = newWidth;
+            newWidth = newWidth*slotWidth; 
+            sel[i].div.style.width = newWidth + "px";
+        }
+    }
+    function onMouseUp(e){
+        document.removeEventListener('mouseup', onMouseUp);
+        document.removeEventListener('mousemove', onMouseMove);
+    }
+}
+
+function drag(e){
+    if (e.target.id[0] != "o"){
+        return;
+    }
+    var target;
+    var tnote;
+    target = e.target;
+    var initX = e.clientX;
+    var initY = e.clientY;
+    tnote = notes[targetToNote(target)];
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    var sel = [tnote];
+    if (selected.length != 0){
+       sel = selected; 
+    }
+    var startX = [];
+    var startY = [];
+    var startOff = [];
+    var startNote = [];
+    for (var i = 0; i < sel.length; i++){
+        startX.push(parseInt(sel[i].div.style.left)); 
+        startY.push(parseInt(sel[i].div.style.top)); 
+        startOff.push(sel[i].offset);
+        startNote.push(sel[i].note);
+    }
+    function onMouseMove(e){
+        for (var i = 0; i < sel.length; i++){
+            var newX = (startX[i] + Math.floor((e.clientX - initX)/slotWidth)*slotWidth);
+            var newY = (startY[i] + Math.floor((e.clientY - initY)/noteHeight)*noteHeight);
+            var offdiff = Math.floor((e.clientX - initX)/slotWidth);
+            var notediff = Math.floor((e.clientY - initY)/noteHeight);
+            sel[i].offset = startOff[i] + offdiff;
+            sel[i].note = startNote[i] + notediff;
+            sel[i].div.id = "o" + sel[i].offset + "n" + sel[i].note;
+            //sel[i].beats = newWidth;
+            sel[i].div.style.left = newX + "px";
+            sel[i].div.style.top = newY + "px";
+        }
     }
     function onMouseUp(e){
         document.removeEventListener('mouseup', onMouseUp);
@@ -335,6 +417,7 @@ function resize(e){
 }
 
 document.getElementById('container').addEventListener('contextmenu', e => {
+    deselect();
 	if (e.target.id[0] != "o"){
 		e.preventDefault();
 		return;
